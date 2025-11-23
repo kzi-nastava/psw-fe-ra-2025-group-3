c5ee59efdbb1a7285009078e8af3e7afc9624356
andjelakumazec <andjelakumazec5@gmail.com>
2025-11-23 02:11:45 +0100

    fix:Dodavanje paginacije prilikom prikaza svih ocena i unapređenje izgleda

--- Explorer/src/app/feature-modules/stakeholders/app-rating-list/app-rating-list.component.ts
+++ Explorer/src/app/feature-modules/stakeholders/app-rating-list/app-rating-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppRatingService } from '../app-rating.service';
-import { AppRatingResponse } from '../model/app-rating.model';
+import { AppRatingResponse, PagedResult } from '../model/app-rating.model';
 
 @Component({
   selector: 'xp-app-rating-list',
@@ -10,6 +10,9 @@ import { AppRatingResponse } from '../model/app-rating.model';
 export class AppRatingListComponent implements OnInit {
 
   ratings: AppRatingResponse[] = [];
+  page: number = 1;
+  pageSize: number = 6;
+  totalCount: number = 0;
 
   constructor(private appRatingService: AppRatingService) { }
 
@@ -17,16 +20,41 @@ export class AppRatingListComponent implements OnInit {
     this.loadRatings();
   }
 
+  get maxPage(): number {
+    return this.totalCount === 0 ? 1 : Math.ceil(this.totalCount / this.pageSize);
+  }
+
   loadRatings(): void {
-    this.appRatingService.getAllRatings().subscribe({
-      next: (result) => {
-        this.ratings = result;
+    this.appRatingService.getAllRatings(this.page, this.pageSize).subscribe({
+      next: (result: PagedResult<AppRatingResponse>) => {
+        this.ratings = result.results;
+        this.totalCount = result.totalCount; 
+
+        if (this.page > this.maxPage) {
+          this.page = this.maxPage;
+          this.loadRatings();
+        }
       },
       error: (err) => {
         console.error('Error while loading ratings', err);
         this.ratings = [];
+        this.totalCount = 0;
       }
     });
+  }
+
+  nextPage(): void {
+    if (this.page < this.maxPage) {
+      this.page++;
+      this.loadRatings();
+    }
+  }
+
+  prevPage(): void {
+    if (this.page > 1) {
+      this.page--;
+      this.loadRatings();
+    }
   }
 }
 
