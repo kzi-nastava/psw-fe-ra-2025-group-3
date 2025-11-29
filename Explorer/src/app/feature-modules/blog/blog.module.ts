ef0034daeac9cf27a850d1f06529cd3bf43b4c6b
c4ps63 <pavlekapuran1@gmail.com>
2025-11-29 19:36:05 +0100

    Image upload and correct UI added

--- Explorer/src/app/feature-modules/blog/blog.module.ts
+++ Explorer/src/app/feature-modules/blog/blog.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
 
 // Angular Material
@@ -25,6 +25,7 @@ import { BlogFormComponent } from './blog-form/blog-form.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     MarkdownModule.forChild(),
     // Material modules
