fe6d933145b66f3a6e9974d8da57b38d0edd6750
Tamara Savic <tamarasavic2003g@gmail.com>
2026-01-07 20:28:11 +0100

    Implementacija prikaza kupovine ture sa ac

--- Explorer/src/app/feature-modules/stakeholders/shopping-cart.service.ts
+++ Explorer/src/app/feature-modules/stakeholders/shopping-cart.service.ts
@@ -1,8 +1,16 @@
+// src/app/feature-modules/stakeholders/shopping-cart.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/env/environment';
 import { ShoppingCart } from './model/shopping-cart.model';
+
+export interface CheckoutResult {
+  success: boolean;
+  message: string;
+  tokens: any[];
+  purchaseRecords: any[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +30,12 @@ export class ShoppingCartService {
 
   removeFromCart(tourId: number): Observable<ShoppingCart> {
     return this.http.delete<ShoppingCart>(`${this.baseUrl}/items/${tourId}`);
+  }
+
+  checkout(): Observable<CheckoutResult> {
+    return this.http.post<CheckoutResult>(
+      environment.apiHost + 'tourist/purchase/checkout',
+      {}
+    );
   }
 }
