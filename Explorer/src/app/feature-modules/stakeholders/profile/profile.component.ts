5e0e09633dc8c161fb52ec2756a1485c23757f78
Njeno Ime <njen@email.com>
2026-01-19 20:57:15 +0100

    feat/ prikaz u my profile autora statistike

--- Explorer/src/app/feature-modules/stakeholders/profile/profile.component.ts
+++ Explorer/src/app/feature-modules/stakeholders/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { StakeholderService } from '../stakeholder.service';
 import { Person } from '../model/person.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { AuthorProfileStatsDto } from '../model/author-profile-stats.model';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
 
 @Component({
   selector: 'xp-profile',
@@ -11,7 +14,12 @@ import { Person } from '../model/person.model';
 export class ProfileComponent implements OnInit {
 
   isEditing = false;
+
   
+  authorStats: AuthorProfileStatsDto | null = null;
+  isAuthor = false;
+  
+
   profileForm = new FormGroup({
     name: new FormControl('', Validators.required),
     surname: new FormControl('', Validators.required),
@@ -21,10 +29,26 @@ export class ProfileComponent implements OnInit {
     profilePictureUrl: new FormControl('')
   });
 
-  constructor(private service: StakeholderService) { }
+ 
+  constructor(
+    private service: StakeholderService,
+    private authService: AuthService
+  ) { }
+ 
 
   ngOnInit(): void {
     this.loadProfile();
+
+    
+    this.authService.user$.subscribe((user: User | undefined) => {
+      this.isAuthor = user?.role === 'author';
+      if (this.isAuthor) {
+        this.loadAuthorStats();
+      } else {
+        this.authorStats = null;
+      }
+    });
+    
   }
 
   loadProfile(): void {
@@ -47,6 +71,20 @@ export class ProfileComponent implements OnInit {
     });
   }
 
+  // ===== DODATO =====
+  loadAuthorStats(): void {
+    this.service.getMyAuthorProfileStats().subscribe({
+      next: (stats) => {
+        this.authorStats = stats;
+      },
+      error: (err) => {
+        console.error('Failed to load author stats:', err);
+        this.authorStats = null;
+      }
+    });
+  }
+  // ==================
+
   toggleEdit(): void {
     if (this.isEditing) {
       this.profileForm.disable();
@@ -65,7 +103,7 @@ export class ProfileComponent implements OnInit {
 
     const updatedProfile: Person = {
       id: 0,
-      userId: 0, 
+      userId: 0,
       name: this.profileForm.value.name || '',
       surname: this.profileForm.value.surname || '',
       email: this.profileForm.value.email || '',
