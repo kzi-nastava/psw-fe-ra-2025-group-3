de779cc7d18a77b8163edfbc047e4fd2835d8eec
unknown <stanacev.l03@gmail.com>
2026-01-20 16:33:51 +0100

    equipment

--- Explorer/src/app/feature-modules/stakeholders/model/tour-details.model.ts
+++ Explorer/src/app/feature-modules/stakeholders/model/tour-details.model.ts
@@ -1,5 +1,12 @@
 import { KeyPoint } from '../../tour-authoring/key-points/model/key-point.model';
 import { TourReview } from '../../tour-execution/model/tour-review.model';   
+import { Equipment } from '../../administration/model/equipment.model';
+
+export interface EquipmentDto {
+  id: number;
+  name: string;
+  description?: string;
+}
 
 export interface TourDetails {
   id: number;
@@ -12,4 +19,5 @@ export interface TourDetails {
   reviews: TourReview[];      // lista recenzija
   
   keyPoints: KeyPoint[] | null;
+  equipment: EquipmentDto[];
 }
---
+++ Explorer/src/app/feature-modules/tour-authoring/model/equipment.model.ts
@@ -0,0 +1,5 @@
+export interface Equipment {
+  id: number;
+  Name: string;      // ← veliko N
+  Description?: string;  // ← veliko D
+}
