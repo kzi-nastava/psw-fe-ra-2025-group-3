4e1393ee693f37971a76983e35fee5507b3f20f2
Petar Bratić <bratic.petar03@gmail.com>
2026-01-27 17:42:04 +0100

    achievements locked/unlocked

--- Explorer/src/app/feature-modules/stakeholders/achievements/achievements.component.ts
+++ Explorer/src/app/feature-modules/stakeholders/achievements/achievements.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AchievementsService, AchievementDto } from './achievements.service';
 
 interface AchievementIcon {
+  code: string;
   title: string;
   iconPath: string;
 }
@@ -10,70 +12,50 @@ interface AchievementIcon {
   templateUrl: './achievements.component.html',
   styleUrls: ['./achievements.component.css']
 })
-export class AchievementsComponent {
+export class AchievementsComponent implements OnInit {
+
+  unlockedCodes = new Set<string>();
 
   toursCompleted: AchievementIcon[] = [
-    {
-      title: '1 tour completed',
-      iconPath: 'assets/achievement-icons/oneTourCompleted.png'
-    },
-    {
-      title: '5 tours completed',
-      iconPath: 'assets/achievement-icons/fiveToursCompleted.png'
-    },
-    {
-      title: '10 tours completed',
-      iconPath: 'assets/achievement-icons/tenToursCompleted.png'
-    }
+    { code: 'FirstTourCompleted', title: '1 tour completed', iconPath: 'assets/achievement-icons/oneTourCompleted.png' },
+    { code: 'FiveToursCompleted', title: '5 tours completed', iconPath: 'assets/achievement-icons/fiveToursCompleted.png' },
+    { code: 'TenToursCompleted', title: '10 tours completed', iconPath: 'assets/achievement-icons/tenToursCompleted.png' }
   ];
 
   tourReviews: AchievementIcon[] = [
-    {
-      title: '1 tour reviewed',
-      iconPath: 'assets/achievement-icons/oneTourReviewed.png'
-    },
-    {
-      title: '5 tours reviewed',
-      iconPath: 'assets/achievement-icons/fiveToursReviewed.png'
-    },
-    {
-      title: '10 tours reviewed',
-      iconPath: 'assets/achievement-icons/tenToursReviewed.png'
-    }
+    { code: 'FirstReviewWritten', title: '1 tour reviewed', iconPath: 'assets/achievement-icons/oneTourReviewed.png' },
+    { code: 'FiveReviewsWritten', title: '5 tours reviewed', iconPath: 'assets/achievement-icons/fiveToursReviewed.png' },
+    { code: 'TenReviewsWritten', title: '10 tours reviewed', iconPath: 'assets/achievement-icons/tenToursReviewed.png' }
   ];
 
   toursBought: AchievementIcon[] = [
-    {
-      title: 'Bought 1 tour',
-      iconPath: 'assets/achievement-icons/boughtOneTour.png'
-    },
-    {
-      title: 'Bought 5 tours',
-      iconPath: 'assets/achievement-icons/boughtFiveTours.png'
-    },
-    {
-      title: 'Bought 10 tours',
-      iconPath: 'assets/achievement-icons/boughtTenTours.png'
-    }
+    { code: 'FirstTourBought', title: 'Bought 1 tour', iconPath: 'assets/achievement-icons/boughtOneTour.png' },
+    { code: 'FiveTourBought', title: 'Bought 5 tours', iconPath: 'assets/achievement-icons/boughtFiveTours.png' },
+    { code: 'TenTourBought', title: 'Bought 10 tours', iconPath: 'assets/achievement-icons/boughtTenTours.png' }
   ];
 
   otherAchievements: AchievementIcon[] = [
-    {
-      title: 'Joined a club',
-      iconPath: 'assets/achievement-icons/joinedClub.png'
-    },
-    {
-      title: 'Created a blog',
-      iconPath: 'assets/achievement-icons/blogCreated.png'
-    },
-    {
-      title: 'Reviewed the app',
-      iconPath: 'assets/achievement-icons/appReviewed.png'
-    },
-    {
-      title: 'Profile picture set',
-      iconPath: 'assets/achievement-icons/profilePictureSet.png'
-    }
+    { code: 'FirstClubJoined', title: 'Joined a club', iconPath: 'assets/achievement-icons/joinedClub.png' },
+    { code: 'FirstBlogCreated', title: 'Created a blog', iconPath: 'assets/achievement-icons/blogCreated.png' },
+    { code: 'FirstAppReview', title: 'Reviewed the app', iconPath: 'assets/achievement-icons/appReviewed.png' },
+    { code: 'FirstProfilePictureSet', title: 'Profile picture set', iconPath: 'assets/achievement-icons/profilePictureSet.png' }
   ];
 
+  constructor(private achievementsService: AchievementsService) {}
+
+  ngOnInit(): void {
+    this.achievementsService.getForCurrentTourist().subscribe({
+      next: (items: AchievementDto[]) => {
+        this.unlockedCodes = new Set(items.map(i => i.code));
+      },
+      error: () => {
+        // ako padne backend, ostaje sve zakljucano (zasivljeno)
+        this.unlockedCodes = new Set<string>();
+      }
+    });
+  }
+
+  isUnlocked(code: string): boolean {
+    return this.unlockedCodes.has(code);
+  }
 }
